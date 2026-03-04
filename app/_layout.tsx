1b5647c59fb91add948a57c6ac52c8eb367efd04
tarkeshwar raj <tarkeshwarraj@gmail.com>
2026-03-04 10:50:23 +0530

    root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,15 @@
+import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return(
+    <SafeAreaView style={{ flex: 1 }}>
+  <Stack
+    screenOptions={{
+      headerShown: false,
+    }}
+    />
+    </SafeAreaView>
+  )
 }
